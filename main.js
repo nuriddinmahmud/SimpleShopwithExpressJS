68cbd5b9493f61ef2760d9d419b2f8d6416de8bc
Will-Marco <mr.marco0101@gmail.com>
2025-03-18 18:09:46 +0500

    wrote commmit in main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 // const swaggerJSDoc = require("swagger-jsdoc");
 // const swaggerUi = require("swagger-ui-express");
 // const mainRoute = require("./routes/index.js");
-const { sequelize } = require("./config/database.js");
+const { sequelize } = require('./config/database.js');
 
 dotenv.config();
 const PORT = process.env.PORT || 3006;
@@ -13,16 +13,18 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
+// Salom
 
 async function shop() {
   try {
     await sequelize.authenticate();
-    console.log("Connected to database successfully ✅");
+    console.log('Connected to database successfully ✅');
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (error) {
     console.log(error.message);
