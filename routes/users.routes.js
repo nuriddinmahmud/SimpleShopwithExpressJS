52534906b9212db26c7d6a664fe76302709fd81f
Nuriddin Mahmud <154328556+nuriddinmahmud@users.noreply.github.com>
2025-03-19 15:50:08 +0500

    nuriddin's wares added

---
+++ routes/users.routes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const {
+  register,
+  verifyOtp,
+  login,
+  findAll,
+  update,
+  remove,
+  promoteToAdmin,
+  findOne,
+} = require("../controllers/users.controller.js");
+const verifyToken = require("../middleware/verifyToken.js");
+const checkRole = require("../middleware/rolePolice.js");
+const selfPolice = require("../middleware/selfPolice.js");
+
+const UsersRouter = express.Router();
+
+UsersRouter.post("/register", register);
+UsersRouter.post("/verify-otp", verifyOtp);
+UsersRouter.post("/login", login);
+UsersRouter.patch(
+  "/promoteToAdmin/:id",
+  verifyToken,
+  selfPolice(["Admin"]),
+  promoteToAdmin
+);
+UsersRouter.get(
+  "/",
+  verifyToken,
+  checkRole(["Admin", "User", "Seller", "SuperAdmin"]),
+  findAll
+);
+UsersRouter.get(
+  "/:id",
+  verifyToken,
+  checkRole(["Admin", "User", "Seller", "SuperAdmin"]),
+  findOne
+);
+UsersRouter.patch("/:id", verifyToken, selfPolice(["Admin"]), update);
+UsersRouter.delete("/:id", verifyToken, selfPolice(["Admin"]), remove);
+
+module.exports = UsersRouter;
